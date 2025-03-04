87e5c7c588878e88a2e7a9ab4cf424c0fce1e1a2
Max <max.tyushkevich@gmail.com>
2025-03-04 22:31:34 +0300

    Added flush of Redis at startup

--- sniffer_svc/api/main.py
+++ sniffer_svc/api/main.py
@@ -1,9 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from api.routers.sniffer import router as sniffer_router
 from api.routers.interface import router as interface_router
+from api.external_services.redis_config import get_redis_connection
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn = get_redis_connection()
+    await conn.flushdb()
+    yield
+    await conn.close()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(sniffer_router)
 app.include_router(interface_router)
