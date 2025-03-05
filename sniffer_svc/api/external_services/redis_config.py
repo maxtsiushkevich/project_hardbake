e2a0230ed4415ad8bef8790dfb07bf8fc814b20c
Max <max.tyushkevich@gmail.com>
2025-03-05 23:21:47 +0300

    Remove some files

--- sniffer_svc/api/external_services/redis_config.py
+++
@@ -1,7 +0,0 @@
-import os
-import redis.asyncio as redis
-
-def get_redis_connection():
-    host = os.getenv("REDIS_HOST")
-    port = os.getenv("REDIS_PORT")
-    return redis.Redis(host=host, port=int(port), db=0)
