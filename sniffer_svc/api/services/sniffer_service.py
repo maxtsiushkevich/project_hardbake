c0b9abb02350a2a6e981dd69de5f3738b83ee47f
Max <max.tyushkevich@gmail.com>
2025-03-09 00:44:32 +0300

    Task storage removed as unnecessary

--- sniffer_svc/api/services/sniffer_service.py
+++ sniffer_svc/api/services/sniffer_service.py
@@ -2,7 +2,7 @@ from uuid import UUID, uuid4
 
 import asyncio
 
-from api.core.context import tasks, sniffers
+from api.core.context import sniffers
 from api.exceptions.exceptions import SniffNotFoundError
 from api.schemas.sniffer import SniffStatus, StartSniffDetails
 from api.repository.redis_repository import RedisRepository
@@ -19,8 +19,7 @@ class SnifferService:
     async def start(self, iface: str):
         sniff_id = uuid4()
         try:
-            task = asyncio.create_task(sniff_task(sniff_id, iface, self.redis))
-            tasks[sniff_id] = task
+            asyncio.create_task(sniff_task(sniff_id, iface, self.redis))
             details = StartSniffDetails(sniff_id=sniff_id, start_at=datetime.now(), interface=iface)
             await self.redis.save_sniff(details)
             return details
@@ -29,21 +28,13 @@ class SnifferService:
 
     async def stop(self, task_id: UUID):
         sniffer = sniffers.get(task_id)
-        task = tasks.get(task_id)
 
-        if not sniffer or not task:
+        if not sniffer:
             raise SniffNotFoundError(f"Sniffer {task_id} not found")
 
         if sniffer:
             sniffer.stop()
             del sniffers[task_id]
-        if task:
-            task.cancel()
-            try:
-                await task
-            except asyncio.CancelledError:
-                pass
-            del tasks[task_id]
 
         result = await self.redis.stop_sniff(task_id)
         return result
