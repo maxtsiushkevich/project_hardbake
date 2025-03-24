8b9f5f0f36db861bd98a76d8be1312aa2aad5d16
Max Tsiushkevich <max.tyushkevich@gmail.com>
2025-03-24 23:17:52 +0300

    Add: created prototypes for working with ports

--- system_info_svc/api/main.py
+++ system_info_svc/api/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from api.monitoring.prometheus import metrics, instrumentator
 from dotenv import load_dotenv
 from api.routers.interface import router as interface_router
+from api.routers.port import router as port_router
 
 load_dotenv()
 
@@ -10,9 +11,10 @@ app = FastAPI(title='Project Hardbake. System Information service')
 
 instrumentator.instrument(app, metric_namespace="system_info_svc").expose(app)
 
-app.mount("/metrics", metrics)
+app.mount("/metrics", metrics, name="Metrics")
 
 app.include_router(interface_router)
+app.include_router(port_router)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8001)
