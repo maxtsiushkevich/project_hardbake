9b89cb8fd8d156bc9aa92a1b65377d0060a903c1
Max Tsiushkevich <max.tyushkevich@gmail.com>
2025-04-06 21:25:39 +0300

    Add. Started dev of Packet Processor Service

---
+++ packet_processor_svc/api/main.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+from api.monitoring.prometheus import metrics, instrumentator
+from dotenv import load_dotenv
+from api.routers.packet_processor import router as packet_router
+
+load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    #shutdown
+
+app = FastAPI(title='Project Hardbake. Packet processor service')
+
+instrumentator.instrument(app, metric_namespace="system_info_svc").expose(app)
+
+app.mount("/metrics", metrics, name="Metrics")
+app.include_router(packet_router)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8001)
