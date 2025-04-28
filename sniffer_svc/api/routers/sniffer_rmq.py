ab47b350b6b84fca5a1ed8f81f825d3cad26f0fc
Max Tsiushkevich <max.tyushkevich@gmail.com>
2025-04-28 22:38:04 +0300

    Add. Added descriptions of possible endpoint responses to sniffer_svc

--- sniffer_svc/api/routers/sniffer_rmq.py
+++ sniffer_svc/api/routers/sniffer_rmq.py
@@ -10,7 +10,16 @@ from api.services.sniffer_service import SnifferService
 router = APIRouter(prefix="/sniffer-rmq", tags=["Sniffer"])
 
 
-@router.post("/start", status_code=status.HTTP_202_ACCEPTED, response_model=StartSniffDetails)
+@router.post("/start",
+             status_code=status.HTTP_202_ACCEPTED,
+             response_model=StartSniffDetails,
+             responses={
+                 202: {"description": "Sniffer successfully launched"},
+                 409: {"description": "Sniffer already running on a given interface"},
+                 500: {"description": "Internal server error"},
+                 503: {"description": "RabbitMQ not available"},
+             }
+             )
 async def start_sniff(iface: str, write_in_file: bool = False, filter_params: SniffFilter | None = None):
     async with RedisConnection() as connection:
         redis = RedisRepository(connection.redis)
@@ -30,7 +39,13 @@ async def start_sniff(iface: str, write_in_file: bool = False, filter_params: Sn
             raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=str(e))
 
 
-@router.patch("/stop", response_model=SniffDetails)
+@router.patch("/stop",
+              response_model=SniffDetails,
+              responses={
+                  200: {"description": "OK"},
+                  404: {"description": "No sniffing session with the specified ID was found"},
+              }
+              )
 async def stop_sniff(sniff_id: UUID):
     async with RedisConnection() as connection:
         redis = RedisRepository(connection.redis)
@@ -44,7 +59,13 @@ async def stop_sniff(sniff_id: UUID):
     return SniffDetails(**sniff.dict())
 
 
-@router.get("/all", response_model=SniffListResponse)
+@router.get("/all",
+            response_model=SniffListResponse,
+            responses={
+                200: {"description": "OK"},
+                204: {"description": "There are no running sniffing sessions"},
+            }
+            )
 async def get_all_sniffs(start_pos: int | None = None, quantity: int | None = None):
     async with RedisConnection() as connection:
         redis = RedisRepository(connection.redis)
@@ -57,7 +78,13 @@ async def get_all_sniffs(start_pos: int | None = None, quantity: int | None = No
     return SniffListResponse(sniffs=results, total=len(results))
 
 
-@router.get("/{task_id}", response_model=SniffDetails)
+@router.get("/{task_id}",
+            response_model=SniffDetails,
+            responses={
+                200: {"description": "OK"},
+                404: {"description": "There are no sniffing session with the specified ID was found"},
+            }
+            )
 async def get_sniff_details(task_id: UUID):
     async with RedisConnection() as connection:
         redis = RedisRepository(connection.redis)
@@ -65,12 +92,18 @@ async def get_sniff_details(task_id: UUID):
 
         result = await sniffer_service.get(task_id)
         if not result:
-            raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Sniff {task_id} not found")
 
     return SniffDetails(**result.dict())
 
 
-@router.get("/status/{status}", response_model=SniffListResponse)
+@router.get("/status/{status}",
+            response_model=SniffListResponse,
+            responses={
+                200: {"description": "OK"},
+                404: {"description": "There are no sniffing session with the specified ID was found"},
+            }
+            )
 async def get_sniffs_by_status(target_status: SniffStatus):
     async with RedisConnection() as connection:
         redis = RedisRepository(connection.redis)
@@ -78,6 +111,6 @@ async def get_sniffs_by_status(target_status: SniffStatus):
 
         results = await sniffer_service.get_by_status(target_status)
         if not results:
-            raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     return SniffListResponse(sniffs=results, total=len(results))
